05c606216518e86c7a32397540faaff9d6db683f
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-04-13 19:45:50 +0100

    returns the same board if player moves left into a wall

--- lib/challenge3.rb
+++ lib/challenge3.rb
@@ -1,5 +1,28 @@
 class Challenge3
+    def initialize
+        @valid_moves = ["U", "D", "L", "R"]
+    end
+
     def processSokobanMove(board, move)
-        return board 
+        @board = board
+        @move = move
+        return board if !@valid_moves.include?(move) || moveToWall? 
+    end
+
+    def moveToWall?
+        player_position = find_player
+        next_square = @board[player_position[:row]][player_position[:column] - 1] if @move == "L"
+        return true if next_square == nil || next_square == "#"
+    end
+
+    def find_player
+        player_position = {}
+        @board.each_with_index do |row, idx|
+            if row.include?("p") || row.include?("P")
+                player_position[:row] = idx
+                row.include?("p") ? player_position[:column] = row.index("p") : row.index("P")
+                return player_position
+            end
+        end
     end
 end
--- spec/challenge3_spec.rb
+++ spec/challenge3_spec.rb
@@ -3,7 +3,11 @@ require './lib/challenge3.rb'
 describe Challenge3 do
     it 'returns the same board if player move is invalid' do
         game = Challenge3.new
-        expect(game.processSokobanMove(['###', '#p#', '###'], 'W')).to eq(['###', '#p#', '###'])
-        
+        expect(game.processSokobanMove(['###', '#p#', '###'], 'W')).to eq(['###', '#p#', '###'])  
+    end
+
+    it 'returns the same board if player moves left into a wall' do
+        game = Challenge3.new
+        expect(game.processSokobanMove(['###', '#p#', '###'], 'L')).to eq(['###', '#p#', '###'])  
     end
 end
